c0b6289bc485aa8c2cb28e2b227b8dc169368c59
Furkan <takci.resmi@gmail.com>
2024-04-06 18:09:05 +0300

    yeni dosyalar eklendi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useEffect } from 'react';
 import logo from './logo.svg';
 
 function App() {
+
+  useEffect(() => {
+    if(process.env.NODE_ENV === 'production') {
+      // analytici bilgilendi
+    }
+  }, [])
+
   return (
     <div className="App">
-      <img src="/logo512.png" alt=""/>
-      <img src={logo} alt="" />
+      <h3>{process.env.NODE_ENV}</h3>
+      <p>
+        {process.env.REACT_APP_API_URL}
+      </p>
+      {process.env.NODE_ENV === 'production' && (
+        <>
+        <img src="/logo512.png" alt=""/>
+        <img src={logo} alt="" />
+        </>
+      )}
     </div>
   );
 }
